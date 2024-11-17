09ca0da2c45c2c4f1f21a225952fadaf82867614
Ivan <vankata294n4@gmail.com>
2024-11-17 13:03:30 +0200

    Improved snapshot generating

--- tests/code/make_snapshots.py
+++ tests/code/make_snapshots.py
@@ -4,5 +4,11 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
 from pybul_converter import custom_translate
 
-custom_translate('./tests/code/simple', True)
-custom_translate('./tests/code/medium', True)
+SNAPSHOTS = [
+    './tests/code/simple',
+    './tests/code/medium'
+]
+
+for SNAPSHOT in SNAPSHOTS:
+    custom_translate(SNAPSHOT, True, False)
+
